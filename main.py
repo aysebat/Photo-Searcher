2992fc9d3a1d65c182331d597f1cddb659535991
aysebat <aaysebatt@gmail.com>
2023-01-08 12:44:22 +0300

    added the search_images method

--- main.py
+++ main.py
@@ -2,10 +2,24 @@ from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
 
+import wikipedia
+import requests
+
 Builder.load_file('frontend.kv')
 class MainScreen(Screen):
     def search_images(self):
-        pass
+        # Get user quey from text input
+        query = self.manager.current_screen.ids.user_query.text
+        #Get Wikipedia images and the first  image url's
+        page = wikipedia.page(query,  auto_suggest=False)
+        image_link = page.images[0]
+        #downloand the images
+        headers ={'User-agent':'Mozilla/5.0'}
+        req = requests.get(image_link, headers=headers)
+        with open ('images/image.jpg', 'wb') as file:
+            file.write(req.content)
+        #put the image in the widget
+        self.manager.current_screen.ids.img.source = 'images/image.jpg'
 
 class RootWidget(ScreenManager):
     pass
