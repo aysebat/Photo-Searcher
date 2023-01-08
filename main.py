953e99e05a6ed3dd795ad2d77396644a8943b401
aysebat <aaysebatt@gmail.com>
2023-01-08 14:17:20 +0300

    reorganize the classs

--- main.py
+++ main.py
@@ -7,19 +7,24 @@ import requests
 
 Builder.load_file('frontend.kv')
 class MainScreen(Screen):
-    def search_images(self):
-        # Get user quey from text input
+    def get_image(self):
+        # Get user query from text input
         query = self.manager.current_screen.ids.user_query.text
         #Get Wikipedia images and the first  image url's
         page = wikipedia.page(query,  auto_suggest=False)
         image_link = page.images[0]
+        return image_link
+    def download_image(self):
         #downloand the images
         headers ={'User-agent':'Mozilla/5.0'}
-        req = requests.get(image_link, headers=headers)
-        with open ('images/image.jpg', 'wb') as file:
+        req = requests.get(self.get_image(), headers=headers)
+        imagePath = 'images/image.jpg'
+        with open (imagePath, 'wb') as file:
             file.write(req.content)
+        return imagePath
+    def set_image(self):
         #put the image in the widget
-        self.manager.current_screen.ids.img.source = 'images/image.jpg'
+        self.manager.current_screen.ids.img.source = self.download_image()
 
 class RootWidget(ScreenManager):
     pass
